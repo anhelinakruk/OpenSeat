4233f56c4a48535767a6e2ffabc67d6ccbe4bb61
Anhelina <krukanhelina06@gmail.com>
2026-05-31 18:44:28 +0200

    Add web frontend (search page with autocomplete and hop   plan)

--- trains/urls.py
+++ trains/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import api
+from . import api, views
 
 urlpatterns = [
+    path("", views.index),
     path("api/stations/", api.station_view),
     path("api/connections/", api.connections_view),
     path("api/journey/", api.journey_view),
